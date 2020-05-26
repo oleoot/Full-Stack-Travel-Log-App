92bb76d60d1bc7adde33144f05c98916a1f161e4
oleoot <oleoot28@gmail.com>
2020-05-26 14:46:00 +0300

    Mongoose schema

---
+++ server/src/models/LogEntry.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const {
+    Schema
+} = mongoose;
+
+const requiredNumber = {
+    type: Number,
+    required: true,
+}
+
+
+const logEntrySchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    comments: String,
+    image: String,
+    rating: {
+        type: Number,
+        min: 0,
+        max: 10,
+        default: 0
+    },
+    latitude: requiredNumber,
+    longitude: requiredNumber,
+    visitDate: {
+        required: true,
+        type: Date,
+    }
+}, {
+    timestamps: true,
+});
+
+module.exports = logEntrySchema

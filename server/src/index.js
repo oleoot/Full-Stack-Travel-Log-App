323c778c3c189da825394e1f569be83edd26cd1e
oleoot <oleoot28@gmail.com>
2020-05-26 13:51:53 +0300

    Basic express server

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express()
+
+
+
+
+
+
+
+const port = process.env.PORT || 5000;
+
+
+app.listen(port, () => {
+    console.log(`Listening at ${port}`)
+})

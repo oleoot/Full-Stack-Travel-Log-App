61afbde09002755893fa2672e9d1c73a4be0ff5d
oleoot <oleoot28@gmail.com>
2020-05-27 12:04:53 +0300

    Handling form & getting data from it

--- client/src/LogEntryForm.js
+++ client/src/LogEntryForm.js
@@ -1,19 +1,27 @@
 import React from 'react';
-
+import { useForm } from "react-hook-form";
 
 const LogEntryForm = () => {
+    const { register, handleSubmit } = useForm();
+
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
     return (
-        <form className="entry-form">
-            <label for="title">Title</label>
-            <input type="text" name="title" required />
-            <label for="comments">Comments</label>
-            <textarea name="title" rows={3}></textarea>
-            <label for="description">Description</label>
-            <textarea name="description" rows={3}></textarea>
-            <label for="image">Image</label>
+        <form onSubmit={handleSubmit(onSubmit)} className="entry-form">
+            <label htmlFor="title">Title</label>
+            <input type="text" name="title" required ref={register} />
+            <label htmlFor="comments">Comments</label>
+            <textarea name="title" rows={3} ref={register}></textarea>
+            <label htmlFor="description">Description</label>
+            <textarea name="description" rows={3} ref={register}></textarea>
+            <label htmlFor="image">Image</label>
             <input type="text" name="image" />
-            <label for="visitDate">Visit Date</label>
-            <input type="date" name="visitDate" />
+            <label htmlFor="visitDate">Visit Date</label>
+            <input type="date" name="visitDate" required ref={register} />
+            <button>Create Entry</button>
         </form>
     )
 }

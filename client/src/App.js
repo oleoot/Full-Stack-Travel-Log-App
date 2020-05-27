57bf46ec77f6dba6f1bc3bc1b9a8fa5d2409ea2b
oleoot <oleoot28@gmail.com>
2020-05-27 10:07:11 +0300

    Listing all database entries on the map using markers

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
-
+import { useState, useEffect } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import { listLogEntries } from './API'
 
 const App = () => {
+  const [logEntries, setLogEntries] = useState([])
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -12,13 +13,46 @@ const App = () => {
     zoom: 3
   });
 
+
+  useEffect(() => {
+    (async () => {
+      const logEntries = await listLogEntries()
+      setLogEntries(logEntries);
+      console.log(logEntries);
+
+    })()
+
+  }, [])
+
   return (
     <ReactMapGL
       {...viewport}
       mapStyle='mapbox://styles/oleoot/ckaoyg5wm0gz81jl2j9dwk4r1'
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
+      onViewportChange={nextViewport => setViewport(nextViewport)}>
+      {
+        logEntries.map(entry => (
+          <Marker key={entry._id} latitude={entry.latitude} longitude={entry.longitude} offsetLeft={-12} offsetTop={-24}>
+            <svg
+              className="marker"
+              style={{
+                width: '24px',
+                height: '24px'
+              }}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle>
+            </svg>
+          </Marker>
+        )
+
+        )
+      }
+
+    </ReactMapGL>
   );
 }
 export default App

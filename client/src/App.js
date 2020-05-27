ad858e2deb9b99ec177d926a5739ccdb25a691eb
oleoot <oleoot28@gmail.com>
2020-05-27 09:17:21 +0300

    Displaying map box on the page

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,17 @@ import ReactMapGL from 'react-map-gl';
 
 const App = () => {
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
+    width: '100vw',
+    height: '100vh',
+    latitude: 50.45466,
+    longitude: 30.5238,
+    zoom: 3
   });
 
   return (
     <ReactMapGL
       {...viewport}
+      mapStyle='mapbox://styles/oleoot/ckaoyg5wm0gz81jl2j9dwk4r1'
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={nextViewport => setViewport(nextViewport)}
     />
